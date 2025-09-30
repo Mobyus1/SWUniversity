86afafe23483f5108916fcbf3bbc59cb15db0dad
Mobyus1 <mobyus1@hotmail.com>
2025-09-30 02:09:03 -0400

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,8 @@
 function HomePage() {
   return <div className="p-6 space-y-4">
     <h1 className="text-3xl font-bold">Welcome to the SWUniversity</h1>
-    <p className="text-lg">This is test text.</p>
+    <p className="text-lg">This is a place where STAR WARS: Unlimited players can test their knowledge of the game they love to play!</p>
+    <p className="text-lg">Please be patient as we continue to develop the site! Thank you and have fun!</p>
   </div>;
 }
 
