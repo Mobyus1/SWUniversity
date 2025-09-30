ba5545397754fcf82663e9ebdc7589da328c7aba
Mobyus1 <mobyus1@hotmail.com>
2025-09-30 02:06:48 -0400

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 function HomePage() {
   return <div className="p-6 space-y-4">
     <h1 className="text-3xl font-bold">Welcome to the SWUniversity</h1>
+    <p className="text-lg">This is test text.</p>
   </div>;
 }
 
