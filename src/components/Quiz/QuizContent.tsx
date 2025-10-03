04d29a5d1ec7c562bdcefb7954eb65822e7850a8
Mario Torres <mariotorresdev@gmail.com>
2025-10-03 16:12:27 -0500

    fix: end screen margins

--- src/components/Quiz/QuizContent.tsx
+++ src/components/Quiz/QuizContent.tsx
@@ -81,7 +81,7 @@ export function QuizContent({
 
   return <div>
   {
-    quizzesCompleted.length === currentQuizSet.length && <div className="text-center mt-64">
+    quizzesCompleted.length === currentQuizSet.length && <div className="text-center m-[35%_10%] lg:m-[15%_10%]">
       <p className="text-2xl md:text-4xl font-bold mb-4 h-32">You've correctly answered every question!</p>
       <button className="btn btn-primary text-lg p-4" onClick={() => {
         setQuizzesCompleted([]);
